9fdd40b57b29f7661e9669d6036d534a980cec51
euronion <42553970+euronion@users.noreply.github.com>
2018-12-17 11:04:32 +0100

    Removed unused library cyorderedDict.

--- setup.py
+++ setup.py
@@ -19,8 +19,7 @@ setup(
     python_requires='>3.0',
     install_requires=['numpy', 'scipy', 'pandas', 'xarray>=0.10.0', 'dask',
                       'rasterio', 'shapely', 'bottleneck',
-                      'toolz',
-                      'cyordereddict ; python_version<'3.5'"],
+                      'toolz'"],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
